869252953a298e2db271d0e7de8364be9bc0c78a
GurtejKanwar <GurtejKanwar@4bd58929-400a-c5bd-ab8a-565694b8340c>
2011-02-22 17:01:13 +0000

    Created main.cpp and made it print some text.

---
+++ trunk/TheNumbersGame/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 22, 2011
+ *      Author: looterguf
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Welcome to the Numbers Game. It does nothing yet." << endl;
+}
